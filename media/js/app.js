5d865252c93296c1100dc95394ad126cc28e2931
sinker <dansinker@gmail.com>
2014-02-25 20:49:22 -0600

    moved rosss rss reading stuff into app.js instead of in the head

--- media/js/app.js
+++ media/js/app.js
@@ -14,3 +14,51 @@ $('.toggle-navigation').on('click', function() {
 	}
 	return false;
 });
+
+
+// this is legacy ross stuff
+
+
+// new mozilla GA account
+    	var _gaq = _gaq || [];
+    	_gaq.push(['_setAccount', 'UA-35433268-15']);
+    	_gaq.push(['_setDomainName', 'opennews.org']);
+    	_gaq.push(['_trackPageview']);
+    	(function() {
+    		var ga = document.createElement('script'); ga.type = 'text/javascript';
+    		ga.async = true;
+    		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    		var s = document.getElementsByTagName('script')[0];
+    		s.parentNode.insertBefore(ga, s);
+    	})();
+
+	google.load("feeds", "1");
+	function initialize() {
+		var feed_urls = $("a.feed_url");
+		$.each(feed_urls, function() {
+			var current = $(this),
+				feed = new google.feeds.Feed(current.attr('href'));
+			feed.setNumEntries(4);
+			feed.load(function(result) {
+				if (!result.error) {
+					var feed_list = "<ul>";
+					for (var i = 0; i < result.feed.entries.length; i++) {
+						var entry = result.feed.entries[i],
+							url = entry.link;
+						feed_list += "<li><a href='" + url + "'>";
+						if (url.indexOf('source') !== -1) {
+							feed_list += entry.title + "</a>";
+						} else {
+							var chunks = entry.title.split(':'),
+							name = chunks.shift();
+							feed_list += chunks.toString() + "</a> by " + name.toString();
+						}
+						feed_list += "</li>";
+					}
+					feed_list += "</ul>";
+					current.parent().before($(feed_list));
+				}
+			});
+		});
+	}
+	google.setOnLoadCallback(initialize);
